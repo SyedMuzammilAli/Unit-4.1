b8b83e1a5fc7db96e4661e67894bb56d99fd6457
Rahul yadav <iamrahulyadav002@gmail.com>
2023-06-23 23:37:46 +0530

    initial commit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { products } from '../product-list/products';
+import { ProductAlertsComponent } from '../product-alerts/product-alerts.component';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = [...products];
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,26 @@
+// top-bar.component.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  template: `
+    <header>
+      <!-- Top bar content -->
+    </header>
+  `,
+  styleUrls: ['./top-bar.component.css']
+})
+export class TopBarComponent {
+  // Component logic and properties
+  
+  constructor() {
+    // Constructor logic
+  }
+  
+  // Methods and event handlers
+  
+  onButtonClick() {
+    // Handle button click event
+  }
+}
